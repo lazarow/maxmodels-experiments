66c5dd34853d9e031a5b0f522a6fb6eecde91117
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2024-09-10 18:17:49 +0200

    fix: fix paths in the unzip script

--- unzip-all.sh
+++ unzip-all.sh
@@ -4,12 +4,12 @@ unzip crossing-minimization-problem/crossing-minimization-problem.zip -d crossin
 unzip maximal-clique-problem/maximal-clique-problem.zip -d maximal-clique-problem
 unzip valves-location-problem/valves-location-problem.zip -d valves-location-problem
 
-unzip max-cut-problem/models/clingo+smodels+maxmodels.zip -d max-cut-problem/models/clingo+smodels+maxmodels
-unzip max-cut-problem/models/dlv.zip -d max-cut-problem/models/dlv
+unzip max-cut-problem/models/clingo+smodels+maxmodels/clingo+smodels+maxmodels.zip -d max-cut-problem/models/clingo+smodels+maxmodels
+unzip max-cut-problem/models/dlv/dlv.zip -d max-cut-problem/models/dlv
 
-unzip minimum-test-set-problem/models/clingo+smodels+maxmodels.zip -d minimum-test-set-problem/models/clingo+smodels+maxmodels
-unzip minimum-test-set-problem/models/dlv.zip -d minimum-test-set-problem/models/dlv
+unzip minimum-test-set-problem/models/clingo+smodels+maxmodels/clingo+smodels+maxmodels.zip -d minimum-test-set-problem/models/clingo+smodels+maxmodels
+unzip minimum-test-set-problem/models/dlv/dlv.zip -d minimum-test-set-problem/models/dlv
 
-unzip precedence-constrained-scheduling/models/clingo.zip -d precedence-constrained-scheduling/models/clingo
-unzip precedence-constrained-scheduling/models/smodels+maxmodels.zip -d precedence-constrained-scheduling/models/smodels+maxmodels
-unzip precedence-constrained-scheduling/models/dlv.zip -d precedence-constrained-scheduling/models/dlv
+unzip precedence-constrained-scheduling/models/clingo/clingo.zip -d precedence-constrained-scheduling/models/clingo
+unzip precedence-constrained-scheduling/models/smodels+maxmodels/smodels+maxmodels.zip -d precedence-constrained-scheduling/models/smodels+maxmodels
+unzip precedence-constrained-scheduling/models/dlv/dlv.zip -d precedence-constrained-scheduling/models/dlv
