88857f557b7ae85352bf69c89dc6db5b3e91c50f
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2024-09-11 16:18:00 +0200

    change the runner script generator

--- runner-script-generator.php
+++ runner-script-generator.php
@@ -1,11 +1,11 @@
 <?php
 $problems = [
-    'crossing-minimization-problem' => [
-        '_default' => __DIR__ . '/crossing-minimization-problem'
-    ],
     'longest-path-problem' => [
         '_default' => __DIR__ . '/longest-path-problem/models/clingo+smodels+maxmodels',
         'dlv' => __DIR__ . '/longest-path-problem/models/dlv'
+    ],
+    'crossing-minimization-problem' => [
+        '_default' => __DIR__ . '/crossing-minimization-problem'
     ],
     'max-cut-problem' => [
         '_default' => __DIR__ . '/max-cut-problem/models/clingo+smodels+maxmodels',
@@ -17,16 +17,16 @@ $problems = [
 ];
 
 $solvers = [
-    'clingo' => 'time timelimit -t240 clingo -q {{FILES}}',
-    'dlv' => 'time timelimit -t240 dlv --silent=2 {{FILES}}',
-    'smodels' => 'time gringo --output=smodels {{FILES}} | timelimit -t240 smodels 0',
-    'time gringo --output=smodels --warn=none {{FILES}} | lp2normal-2.27 | timelimit -t240 maxmodels -e ~/workspace/asp-solvers/maxmodels/.env'
+    'clingo' => 'timelimit -t240 clingo -q {{FILES}}',
+    'dlv' => 'timelimit -t240 dlv --silent=2 {{FILES}}',
+    'smodels' => 'gringo --output=smodels {{FILES}} | timelimit -t240 smodels 0',
+    'maxmodels' => 'gringo --output=smodels --warn=none {{FILES}} | lp2normal-2.27 | timelimit -t240 maxmodels -e ~/workspace/asp-solvers/maxmodels/.env'
 ];
 
 echo "#!/bin/bash\n\n";
 foreach ($problems as $problem => $paths) {
-    foreach ($solvers as $solver => $command) {
-        for ($i = 1; $i <= 10; $i++) {
+    for ($i = 1; $i <= 10; $i++) {
+        foreach ($solvers as $solver => $command) {
             $path = $paths[$solver] ?? $paths['_default'];
             $files = [];
             if (file_exists("$path/p$i.lp")) {
@@ -38,8 +38,10 @@ foreach ($problems as $problem => $paths) {
             if (file_exists("$path/encoding.asp")) {
                 $files[] = realpath($path) . "/encoding.asp";
             }
+            echo "start=`date +%s%N`\n";
             echo strtr($command, ['{{FILES}}' => implode(' ', $files)]) . "\n";
-            echo "echo '===^^^=== $problem | p$i | $solver ===^^^==='\n";
+            echo "end=`date +%s%N`\n";
+            echo 'echo "===^^^=== '.$problem.' | p'.$i.' | '.$solver.' | It takes $(( ($end - $start) / 1000000 )) milliseconds to complete this task ===^^^==="' . "\n";
             echo "echo ''\n";
         }
     }
